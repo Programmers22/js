8320ef5ec5be451978c3dc115db8216ed295fc36
AmazingWebistes <programowaniezadania@gmail.com>
2021-12-16 16:19:32 +0100

    s

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-function przelicz(){
-    var dor = document.getElementById("d1").value;
-    var dz = document.getElementById("d2").value;
-    var dni = document.getElementById("d3").value;
-    if(!isNaN(dor) && !isNaN(dz) && !isNaN(dni) && dor != "" && dz != "" && dni != ""){
-        var koszt = (dor*35+dz*25)*dni;
-        document.getElementById("wynik").innerHTML ="Dokonales rezerwacji na "+dni+" dni <br> dla "+dor+" doroslych i "+dz+" dzieci.<br>Koszt calkowity noclegow wynosi "+koszt+"zl.";
-        document.getElementById("wynik").style.display = "block";
-    }
-    
-};
 
-document.addEventListener('contextmenu', event => event.preventDefault());
+
